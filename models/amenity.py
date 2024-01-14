ab2913377651d77eee80be3d9e0b6e53637a3557
Malack15 <malackokeiga@gmail.com>
2024-01-14 22:49:58 +0300

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-Module for Amenity class
-"""
+"""Module for Amenity class."""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Custom amenity class
-
-    Attributes:
-        name(str): amenity name
-
-    """
+    """Class representing a Amenity."""
     name = ""
